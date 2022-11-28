ee637850bf2ab798d6939bc5372b814dfbbce592
isado <isadorabellaguarda@gmail.com>
2022-11-28 15:45:05 -0300

    primeiro commit

---
+++ src/main/java/teste/testeanimais/TesteAnimais.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package teste.testeanimais;
+
+import java.util.Scanner;
+
+public class TesteAnimais {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner ( System.in);
+       
+        int distBob, distRex;
+
+        System.out.print("Informe onde esta o gato, Oli: ");
+        int oli = ler.nextInt();
+        System.out.print("Onde está o cão Rex? ");
+        int rex = ler.nextInt();
+        System.out.print("Onde está o cão Bob? ");
+        int bob = ler.nextInt();
+       
+        if (oli<0 || rex<0 || bob<0) {
+            System.out.print("Por favor, digite apenas números positivos\n");
+        }else{
+
+            distBob = oli-bob;
+            distRex = oli-rex;
+
+           /*
+            System.out.println("oli " +oli);
+            System.out.println("rex " +rex);
+            System.out.println("bob " +bob);
+            System.out.println("dist bob " +distBob);
+            System.out.println("dist rex " +distRex);
+            */
+
+            if (distBob<0) {
+                distBob = distBob*(-1);
+            }
+            if (distRex<0) {
+                distRex = distRex*(-1);
+            }
+
+            if (distBob==distRex) {
+                System.out.print("Oli\n");
+            }else if (distBob>distRex) {
+                System.out.print("Rex\n");
+
+            }else  {
+                System.out.println("Bob\n");
+            }
+        }
+
+    }
+}
